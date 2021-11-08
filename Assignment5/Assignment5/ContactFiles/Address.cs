c445d8fb09d95ac19c748a18189a27f40f125fbc
larscjensen <lars.c.jensen@gmail.com>
2021-11-08 19:31:51 +0100

    Ass5

---
+++ Assignment5/Assignment5/ContactFiles/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5.ContactFiles
+{
+    class Address
+    {
+        public Address()
+        {
+
+        }
+        public Address()
+        {
+
+        }
+        public Address()
+        {
+
+        }
+    }
+}
---
+++ Assignment5/Assignment5/ContactFiles/Contact.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5.ContactFiles
+{
+    class Contact
+    {
+        /// <summary>
+        /// Default constructor of class Contact
+        /// </summary>
+        public Contact() // THIS!!!
+        {
+
+        }
+
+        public Contact(Contact toCopy)
+        {
+
+        }
+        public Contact(string firstName, string lastName, Address address, Phone phone, Email email)
+        {
+
+        }
+
+
+        private string firstName;
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (value != "")
+                {
+                    firstName = value;
+                }
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (value != "")
+                {
+                    lastName = value;
+                }
+            }
+        }
+
+        private Address address;
+        public Address AddressData
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                if(value != null)
+                {
+                    address = value;
+                }
+            }
+        }
+        private Email email;
+        public Email EmailData
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    email = value;
+                }
+            }
+        }
+
+        private Phone phone;
+        public Phone PhoneData
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    phone = value;
+                }
+            }
+        }
+
+
+
+
+
+
+    }
+}
